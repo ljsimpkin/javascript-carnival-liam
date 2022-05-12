c0738fc3d3fcc323d461c9acdc372646f9f3f550
ljsimpkin@gmail.com <sandbox1@Sands-MacBook-Pro.local>
2022-05-13 08:38:47 +1200

    change clown clothes"

--- dress-the-clown/dress-the-clown.js
+++ dress-the-clown/dress-the-clown.js
@@ -1,5 +1,46 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Dress The Clown!")
+console.log('Dress The Clown!')
+
+var verticalIndex = 0
+var bodyParts = ['head', 'body', 'shoes']
+// var bodyParts = ['shoes', 'body', 'head']
+var indexArray = [0, 0, 0]
+
+document.onkeydown = checkKey
+
+function checkKey(e) {
+  e = e || window.event
+
+  if (e.keyCode == '38') {
+    // up arrow
+    if (verticalIndex > 0) {
+      verticalIndex = (verticalIndex - 1) % 3
+    }
+  } else if (e.keyCode == '40') {
+    // down arrow
+    verticalIndex = (verticalIndex + 1) % 3
+  } else if (e.keyCode == '37') {
+    // left arrow
+    changeClown((indexArray[verticalIndex] -= 1))
+  } else if (e.keyCode == '39') {
+    // right arrow
+    changeClown((indexArray[verticalIndex] += 1))
+  }
+
+  console.group('arrowKeys')
+  console.log(verticalIndex)
+  console.log(indexArray)
+}
+
+function changeClown(index) {
+  var headSrc =
+    './images/' +
+    bodyParts[Math.abs(verticalIndex)] +
+    (Math.abs(index) % 6) +
+    '.png'
+
+  document.getElementById(bodyParts[Math.abs(verticalIndex)]).src = headSrc
+}
--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -1,5 +1,23 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Inflate The Unicorn!")
+console.log('Inflate The Unicorn!')
+
+let unicorns = [
+    './images/unicorn-0.png',
+    './images/unicorn-1.png',
+    './images/unicorn-2.png',
+    './images/unicorn-3.png',
+]
+
+let clickCount = 1
+
+function changeImage(img) {
+    if (clickCount < 4) {
+        document.getElementById('inflate-an-image').src = unicorns[clickCount]
+        clickCount += 1
+    } else {
+        alert('Unicorn Number ' + clickCount + ' says thank you!')
+    }
+}
