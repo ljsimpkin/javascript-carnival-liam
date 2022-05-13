e0cd4cd0ff4f7267ab9a0b099bcaa80a091eb141
ljsimpkin@gmail.com <sandbox1@Sands-MacBook-Pro.local>
2022-05-13 12:03:29 +1200

    add whack a mole game!

--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -1,5 +1,54 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Whack-a-Mole!")
+console.log('Whack-a-Mole!')
+
+// table = []
+
+// for (let i = 0; i < 25; i++) {
+//     table.push(0)
+// }
+
+document.addEventListener('DOMContentLoaded', start)
+
+function start() {
+    bindEventListeners(document.getElementsByClassName('table')[0].children)
+    addMole(randomIntFromInterval(0, 24).toString())
+}
+
+function bindEventListeners(table) {
+    for (var i = 0; i < table.length; i++) {
+        table[i].addEventListener('click', updateMoleMap)
+            // dots[i].addEventListener('dblclick', undoDot)
+    }
+}
+
+function addMole(tableId) {
+    var img = document.createElement('img')
+    img.src = './mole.PNG'
+    img.id = 'moleImg'
+    document.getElementById(tableId).appendChild(img)
+}
+
+function isMole(tableId) {
+    return tableId == 'moleImg'
+}
+
+function removeMole(tableId) {
+    let d_nested = document.getElementById(tableId)
+    let d = d_nested.parentElement
+    d.removeChild(d_nested)
+}
+
+function updateMoleMap(event) {
+    if (isMole(event.target.id)) {
+        removeMole(event.target.id)
+        addMole(randomIntFromInterval(0, 24).toString())
+    }
+}
+
+function randomIntFromInterval(min, max) {
+    // min and max included
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
